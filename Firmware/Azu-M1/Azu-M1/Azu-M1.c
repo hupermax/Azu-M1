e78d94a9a6c42db7a3d27db68ee19352f481498a
Raman Tkachou <hupermax@gmail.com>
2016-11-02 20:42:27 +0300

    Azu-1

---
+++ Firmware/Azu-M1/Azu-M1/Azu-M1.c
@@ -0,0 +1,17 @@
+/*
+ * Azu_M1.c
+ *
+ * Created: 02.11.2016 20:16:47
+ *  Author: Magistor
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
